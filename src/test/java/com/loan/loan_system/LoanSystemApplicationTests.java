52640f9cfdb538cbca78ab45f74087ed6a35ef0b
khisan <khisan8@gmail.com>
2026-09-03 21:44:13 +0700

    feat: initialize Spring Boot project with basic configuration and User entity

---
+++ src/test/java/com/loan/loan_system/LoanSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loan.loan_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoanSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
