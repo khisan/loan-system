892f306611f9ab27f6d32484886e8d783f0a407b
khisan <khisan8@gmail.com>
2026-09-14 05:34:01 +0700

    feat: implement UserResponse DTO and update UserController to return user details by email

--- src/main/java/com/loan/loan_system/controller/UserController.java
+++ src/main/java/com/loan/loan_system/controller/UserController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.loan.loan_system.service.UserService;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.http.ResponseEntity;
+import com.loan.loan_system.dto.UserResponse;
 
 @RestController
 @RequestMapping ("/api/users")
@@ -22,7 +23,8 @@ public class UserController {
       return ResponseEntity.badRequest().body(Map.of("error", "Email tidak boleh kosong"));
     }
     if (userService.getUserByEmail(email) != null) {
-      return ResponseEntity.ok(userService.getUserByEmail(email));
+      UserResponse userResponse = new UserResponse(userService.getUserByEmail(email).getEmail(), userService.getUserByEmail(email).getRole());
+      return ResponseEntity.ok(userResponse);
     } else {
       return ResponseEntity.status(404).body(Map.of("error", "User tidak ditemukan"));
     }
